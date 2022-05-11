c5db0fcbf80a8c227989b99f3a9f7caffb96fee4
grzeznx2 <grzeznx2@gmail.com>
2022-05-11 04:12:40 +0200

    feat: add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,59 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
+import { FlavorsUnitsComponent } from './features/flavors-units/flavors-units.component';
+import { OrderCreatorComponent } from './features/order/order-creator/order-creator.component';
+import { OrderSummaryComponent } from './features/order/order-summary/order-summary.component';
+import { UserListComponent } from './features/user/user-list/user-list.component';
+import { AuthGuard, RequiredAuthStatus } from './shared/guards/auth.guard';
+import { RedirectGuard } from './shared/guards/redirect.guard';
+import { RoleGuard } from './shared/guards/role.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'order-creator',
+    component: OrderCreatorComponent,
+  },
+  {
+    path: 'register',
+    canActivate: [AuthGuard],
+    data: {
+      requiredAuthStatus: RequiredAuthStatus.UNAUTHENTICATED,
+    },
+    component: RegisterComponent,
+  },
+  {
+    path: 'login',
+    canActivate: [AuthGuard],
+    data: {
+      requiredAuthStatus: RequiredAuthStatus.UNAUTHENTICATED,
+    },
+    component: LoginComponent,
+  },
+  {
+    path: 'orders',
+    component: OrderSummaryComponent,
+  },
+  {
+    path: 'users',
+    canActivate: [RoleGuard],
+    data: { requiredRole: 'ADMIN' },
+    component: UserListComponent,
+  },
+  {
+    path: 'flavors-units',
+    component: FlavorsUnitsComponent,
+  },
+  {
+    path: '',
+    canActivate: [RedirectGuard],
+    component: LoginComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
